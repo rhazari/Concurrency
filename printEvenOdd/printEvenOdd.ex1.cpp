319e5b263dd97a5b288f470df7369eff116b13d3
rhazari <raihan.hazarika@gmail.com>
2023-01-02 12:58:04 -0800

    print Even Odd multi-threaded

--- printEvenOdd/printEvenOdd.ex1.cpp
+++ printEvenOdd/printEvenOdd.ex1.cpp
@@ -2,15 +2,12 @@
 #include <thread>
 #include <condition_variable>
 #include <mutex>
-#include <atomic>
 
 std::mutex mx;
 std::condition_variable cv;
 
 constexpr int LIMIT = 100;
 int val = 1;
-
-std::atomic<int> atomic_val(1);
 
 void printEven(){
     while(val <= LIMIT){
@@ -43,24 +40,6 @@ void printOdd(){
     }
 }
 
-void printEvenWithAtomic(){
-    while(atomic_val <= LIMIT){
-        if(atomic_val % 2 == 0 && atomic_val <= LIMIT){
-            std::cout<<atomic_val<<" ";
-            atomic_val++;
-        }
-    }
-}
-
-void printOddWithAtomic(){
-    while(atomic_val <= LIMIT){
-        if(atomic_val % 2 == 1 && atomic_val <= LIMIT){
-            std::cout<<atomic_val<<" ";
-            atomic_val++;
-        }
-    }
-}
-
 int main(){
     std::cout<<"Sync with locks\n";
     const auto start1 = std::chrono::steady_clock::now();
@@ -70,18 +49,7 @@ int main(){
     th1.join();
     th2.join();
     const std::chrono::duration<double> duration1 = std::chrono::steady_clock::now() - start1;
-    std::cout<<"\nTime for mySumition " << duration1.count() << " seconds" << std::endl;
-    std::cout<<"\n";
-
-    std::cout<<"Sync with atomics\n";
-    const auto start2 = std::chrono::steady_clock::now();
-    std::thread th3(printEvenWithAtomic);
-    std::thread th4(printOddWithAtomic);
-
-    th3.join();
-    th4.join();
-    const std::chrono::duration<double> duration2 = std::chrono::steady_clock::now() - start2;
-    std::cout<<"\nTime for mySumition " << duration2.count() << " seconds" << std::endl;
+    std::cout<<"\nTime elapsed " << duration1.count() << " seconds" << std::endl;
     std::cout<<"\n";
 
     return 0;
---
+++ printEvenOdd/printEvenOdd.ex2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <thread>
+#include <atomic>
+
+constexpr int LIMIT = 100;
+int val = 1;
+
+std::atomic<int> atomic_val(1);
+
+void printEvenWithAtomic(){
+    while(atomic_val <= LIMIT){
+        if(atomic_val % 2 == 0 && atomic_val <= LIMIT){
+            std::cout<<atomic_val<<" ";
+            atomic_val++;
+        }
+    }
+}
+
+void printOddWithAtomic(){
+    while(atomic_val <= LIMIT){
+        if(atomic_val % 2 == 1 && atomic_val <= LIMIT){
+            std::cout<<atomic_val<<" ";
+            atomic_val++;
+        }
+    }
+}
+
+int main(){
+    std::cout<<"Sync with atomics\n";
+    const auto start2 = std::chrono::steady_clock::now();
+    std::thread th3(printEvenWithAtomic);
+    std::thread th4(printOddWithAtomic);
+
+    th3.join();
+    th4.join();
+    const std::chrono::duration<double> duration2 = std::chrono::steady_clock::now() - start2;
+    std::cout<<"\nTime elapsed " << duration2.count() << " seconds" << std::endl;
+    std::cout<<"\n";
+
+    return 0;
+}
